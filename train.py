afca1ad0362287d2f8998f82df1291896558d486
CodyKat <piepie1346@gmail.com>
2024-02-28 17:44:36 +0900

    TODO : set step size

--- train.py
+++ train.py
@@ -5,7 +5,6 @@ from matplotlib import style
 
 
 def parse_data():
-    data_size = 0
     data = []
     with open("data.csv", mode='r') as data_file:
         line = data_file.readline()
@@ -17,34 +16,44 @@ def parse_data():
             line[1] = line[1].strip()
             list_line = [int(i) for i in line]
             data.append(list_line)
-            data_size += 1
     data_file.close()
-    return data, data_size
+    return data
 
 
 # Adjusted the learning rate for example
-def linear_regression(learn_rate=1e-10, iteration=10):
-    data, data_size = parse_data()
+def linear_regression(learn_rate=1e-10):
+    data = parse_data()
     delta0 = 0
     delta1 = 0
+    prev_delta0_grad = 0
+    prev_delta1_grad = 0
     X, Y = zip(*data)
 
     X = np.array(X)
     Y = np.array(Y)
+    prevY = np.array(Y)
 
-    for epoch in range(iteration):
+    while 1:
         Y_predict = delta1 * X + delta0
 
-        # error = np.abs(Y_predict - Y).mean()
-        # if error < 0.001:
-        #     break
+        error = np.abs(Y_predict - Y).mean()
+        if error < 0.001:
+            break
 
         delta0_grad = learn_rate * (Y_predict - Y).mean()
         delta1_grad = learn_rate * ((Y_predict - Y) * X).mean()
-        print(delta0_grad, delta1_grad)
 
         delta0 = delta0 - delta0_grad
         delta1 = delta1 - delta1_grad
+
+        print(Y_predict - prevY)
+        print("\n")
+        print(delta0 - prev_delta0_grad)
+        learn_rate = (Y_predict - prevY).T @ (delta0 - prev_delta0_grad)
+
+        prev_delta0_grad = delta0_grad
+        prev_delta1_grad = delta1_grad
+        prevY = Y_predict
 
     with open("result", 'w') as file:
         file.write(str(delta0))
