a1343e640f871331630cfda0e227dff5fc2f6469
CodyKat <piepie1346@gmail.com>
2024-02-29 16:42:33 +0900

    why not working...

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from train import linear_regression
+import numpy as np
 
 
 def main():
-    delta0 = 0
-    delta1 = 0
     mileage = int(input())
-    linear_regression()
+    # delta = np.array([0.0, 0.0])
+    delta = linear_regression()
 
-    meta_data = open('result', 'r')
+    # meta_data = open('result', 'r')
 
-    delta0 = float(meta_data.readline())
-    delta1 = float(meta_data.readline())
-    print(delta0 + delta1 * mileage)
+    # delta0 = float(meta_data.readline())
+    # delta1 = float(meta_data.readline())
+    print(delta[0] + delta[1] * mileage)
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -21,47 +21,41 @@ def parse_data():
 
 
 # Adjusted the learning rate for example
-def linear_regression(learn_rate=1e-10):
-    data = parse_data()
-    delta0 = 0
-    delta1 = 0
-    prev_delta0_grad = 0
-    prev_delta1_grad = 0
-    X, Y = zip(*data)
+def linear_regression(learn_rate=1e-3):
+    delta = np.array([0.0, 0.0])
+    prev_grad = np.array([0.0, 0.0])
+    prev_delta = np.array([0.0, 0.0])
+
+    pos = parse_data()
+    X, Y = zip(*pos)
 
     X = np.array(X)
     Y = np.array(Y)
-    prevY = np.array(Y)
 
-    while 1:
-        Y_predict = delta1 * X + delta0
+    for i in range(1000):
+        Y_pred = delta[1] * X + delta[0]
 
-        error = np.abs(Y_predict - Y).mean()
-        if error < 0.001:
+        grad = np.array([
+            (Y_pred - Y).mean(),
+            ((Y_pred - Y) * X).mean()
+        ])
+#       Y_pred는 현재 직선에서 예측한 모든 예측점들의 모임임 그러니까 직선이라고 볼 수 있음
+
+        s = delta - prev_delta  # 파라미터 차이 벡터
+        y = grad - prev_grad  # 그라디언트 차이 벡터
+        print("s shape : ", s.shape)
+        print("y shape : ", y.shape)
+        print("np.dot(s, y) : ", np.dot(s, y))
+        print(np.linalg.norm(y))
+
+        if i != 0:
+            learn_rate = np.abs(np.dot(s, y) / np.linalg.norm(y)**2)
+        # 이전 그라디언트 업데이트
+        prev_delta = np.copy(delta)
+        prev_grad = np.copy(grad)
+        delta -= learn_rate * grad
+        print("learn_rate : ", learn_rate)
+        if learn_rate < 1e-10:
             break
-
-        delta0_grad = learn_rate * (Y_predict - Y).mean()
-        delta1_grad = learn_rate * ((Y_predict - Y) * X).mean()
-
-        delta0 = delta0 - delta0_grad
-        delta1 = delta1 - delta1_grad
-
-        print(Y_predict - prevY)
-        print("\n")
-        print(delta0 - prev_delta0_grad)
-        learn_rate = (Y_predict - prevY).T @ (delta0 - prev_delta0_grad)
-
-        prev_delta0_grad = delta0_grad
-        prev_delta1_grad = delta1_grad
-        prevY = Y_predict
-
-    with open("result", 'w') as file:
-        file.write(str(delta0))
-        file.write("\n")
-        file.write(str(delta1))
-
-    plt.scatter(X, Y)
-    plt.scatter(X, Y_predict)
-    plt.show()
-
-    return delta0, delta1
+    print(delta)
+    return delta
